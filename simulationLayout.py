e1591b8289e290fd0780c15fa8d59b0970e86b57
George Yazji <yazjig@mcmaster.ca>
2022-03-28 00:41:18 -0400

    Update simulationLayout.py

--- simulationLayout.py
+++ simulationLayout.py
@@ -420,60 +420,34 @@ class Intersection:
         self.update()
 
         
-    ############# ignore the draw function for now ###################
     def draw():
-        width=36
-        height=30
+        width=24
+        height=24
+        #y for height
+        #x for width
         #¦
         for y in range(0,height+1):
-            for x in range(0,width):
+            for x in range(0,width+1):
                 #upper vertical part
-                if((y>=0 and y<(height/3)) and (x==width/3 or x==width*2/3)):
+                if((6>y>=0 or y>18) and (x==5 or x==17)):
                     print("█",end="")
-                #horizontal part
-                elif((y==height/3 or y==height*2/3) and (x<width/3-1 or x>width*2/3)):
+                elif((6>y>0 or 24>y>18) and (x==11)):
+                    print("|",end="")
+                elif((6>y>=0 or y>18) and (x==7 or x==9 or x==13 or x==15)):
+                    print("¦",end="")
+                elif((6>y>=0 or y>18) and x<6):
+                    print(" ",end="  ")
+                elif ((y==6 or y==18) and (x<6 or 18>x>11)):
                     print("",end=" ")
-                #lower vertical part
-                elif(y>height*2/3 and y<=height) and (x==width/3 or x==width*2/3):
-                    print("█",end="")
-                #vertical island
-                elif (x==width/2 and (1<y<(height/3) or height>y>height*2/3)):
-                    print("▩",end="")
-                #hroizontal island
-                elif (y==height/2 and (1<x<(width/3) or x>(width*2/3))):
-                    print("▩",end=" ")
-                #first lane line from top left
-                elif (y>=0 and y<(height/3) and (x==int((((width/2)-(width/3))*1/3)+width/3))):
-                    print(" ¦ ",end="")
-                #second lane line from top left
-                elif (y>=0 and y<(height/3) and (x==int((((width/2)-(width/3))*2/3)+width/3))):
-                    print(" ¦ ",end="")
-                #third lane line from top left
-                elif (y>=0 and y<(height/3) and (x==int((((width*2/3)-(width/2))*1/3)+width/2))):
-                    print(" ¦ ",end="")
-                #fourth lane line from top left
-                elif (y>=0 and y<(height/3) and (x==int((((width*2/3)-(width/2))*2/3)+width/2))):
-                    print(" ¦ ",end="")
-                elif (y>height*2/3 and y<(height) and (x==int((((width/2)-(width/3))*1/3)+width/3))):
-                    print(" ¦ ",end="")
-                #second lane line from top left
-                elif (y>height*2/3 and y<(height) and (x==int((((width/2)-(width/3))*2/3)+width/3))):
-                    print(" ¦ ",end="")
-                #third lane line from top left
-                elif (y>height*2/3 and y<(height) and (x==int((((width*2/3)-(width/2))*1/3)+width/2))):
-                    print(" ¦ ",end="")
-                #fourth lane line from top left
-                elif (y>height*2/3 and y<(height) and (x==int((((width*2/3)-(width/2))*2/3)+width/2))):
-                    print(" ¦ ",end="")    
-                #white space inside vertically
-                elif ((x>=width/3 and x<=width*2/3) and (y<=height/3 or y>=height*2/3)):
-                    print(" ",end="")
-                elif ((x<=width/3 or x>=width*2/3) and (y>=height/3 and y<=height*2/3)):
-                    print(" ",end="")
-                #print the white space for outer intersection area
-                else:
+                elif (y==12 and (1<x<7 or 20>x>14)):
+                    print("-",end=" ")
+                elif ((x<7 or 22>x>14) and (y==8 or y==10 or y==14 or y==16)):
+                    print("--",end="")
+                elif(y>5 and y<19):
                     print(" ",end=" ")
-            print("\n")
+                else:
+                    print(" ",end="") 
+            print("")
 
     def display(self):
         self.update()
